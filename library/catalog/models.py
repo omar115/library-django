d79c87a326bc34679b66b7f72311e42b54785354
omar115 <omarhasan115@gmail.com>
2021-04-11 18:31:22 +0600

    setting the model

---
+++ library/catalog/models.py
@@ -0,0 +1,41 @@
+from typing import get_type_hints
+from django.db import models
+from django.db.models.deletion import SET_NULL
+from django.urls import reverse #used to generate urls by reversing urlpatterns
+import uuid
+
+# Create your models here.
+
+class Genre(models.Model):
+
+    name = models.CharField(max_length=200, help_text='enter a book genre')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    #book can have one author but author can have multiple books => foreign key
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='write a short description of the book')
+
+    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='write the 13 digit code')
+
+    #genre can cover many books, and books can have many genres
+
+    genre = models.ManyToManyField(Genre, help_text='select a genre for this book')
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        #return the url to get detail record of the book
+        return reverse('book-detail', args=[str(self.id)])
+
+    
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
