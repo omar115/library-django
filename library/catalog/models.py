0f109d976ebc3d06d4057eed899aa07409909154
omar115 <omarhasan115@gmail.com>
2021-04-11 23:05:26 +0600

    registering models with admin

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -1,51 +1,90 @@
-from typing import get_type_hints
 from django.db import models
-from django.db.models.deletion import SET_NULL
-from django.urls import reverse #used to generate urls by reversing urlpatterns
-import uuid
 
 # Create your models here.
 
-class Genre(models.Model):
+from django.urls import reverse  # To generate URLS by reversing URL patterns
 
-    name = models.CharField(max_length=200, help_text='enter a book genre')
+
+class Genre(models.Model):
+    """Model representing a book genre (e.g. Science Fiction, Non Fiction)."""
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+        )
 
     def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
         return self.name
 
 
 class Book(models.Model):
-
+    """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
-
-    #book can have one author but author can have multiple books => foreign key
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
-    summary = models.TextField(max_length=1000, help_text='write a short description of the book')
-
-    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='write the 13 digit code')
-
-    #genre can cover many books, and books can have many genres
-
-    genre = models.ManyToManyField(Genre, help_text='select a genre for this book')
-
-    def __str__(self):
-        return self.title
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in file.
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     
+    class Meta:
+        ordering = ['title', 'author']
+
+    def display_genre(self):
+        """Creates a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
+
     def get_absolute_url(self):
-        #return the url to get detail record of the book
+        """Returns the url to access a particular book instance."""
         return reverse('book-detail', args=[str(self.id)])
 
-    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+
+import uuid  # Required for unique book instances
+from datetime import date
+
+from django.contrib.auth.models import User  # Required to assign User as a borrower
+
+
 class BookInstance(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='unique id for this particular book')
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On Loan'),
+        ('d', 'Maintenance'),
+        ('o', 'On loan'),
         ('a', 'Available'),
         ('r', 'Reserved'),
     )
@@ -54,13 +93,32 @@ class BookInstance(models.Model):
         max_length=1,
         choices=LOAN_STATUS,
         blank=True,
-        default='m',
-        help_text='Book availability',
-    )
+        default='d',
+        help_text='Book availability')
 
     class Meta:
-        ordering = ['due back']
-    
+        ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
     def __str__(self):
-        #string for representing the Model object
-        return f'{self.id} ({self.book.title})'
+        """String for representing the Model object."""
+        return '{0} ({1})'.format(self.id, self.book.title)
+
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return '{0}, {1}'.format(self.last_name, self.first_name)
