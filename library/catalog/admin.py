08495fa38be197e19cd676bfade7e29f788622a6
omar115 <omarhasan115@gmail.com>
2021-04-12 01:55:38 +0600

    bug fix register language in admin panel

--- library/catalog/admin.py
+++ library/catalog/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book)
 admin.site.register(BookInstance)
-
+admin.site.register(Language)
