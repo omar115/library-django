63fd9c40eafa53a1a6a1803ee9f9eac5f3836b4f
omar115 <omarhasan115@gmail.com>
2021-04-14 17:39:13 +0600

    add login option in frontend and password reset option

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# from django.contrib.auth.models import User
 
 from django.urls import reverse  # To generate URLS by reversing URL patterns
 
